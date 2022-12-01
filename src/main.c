3f90bcb763dbef1cfc1de9f7119dcea4d99cc1e7
Hugo Lizarraga <lizarragahugo@gmail.com>
2022-11-30 22:13:39 -0300

    Ver_04 Funciones Generales de la PANTALLA OK!

--- src/main.c
+++ src/main.c
@@ -39,112 +39,90 @@
 
 #include "bsp.h"
 #include <stdbool.h>
-#include "poncho.h"
-#include "chip.h"
+#include "pantalla.h"
+
 
 /* === Macros definitions ====================================================================== */
-//Definiciones de bits asociados a cada segmento para construir los digitos
-#define SEGMENT_A (1 << 0)
-#define SEGMENT_B (1 << 1)
-#define SEGMENT_C (1 << 2)
-#define SEGMENT_D (1 << 3)
-#define SEGMENT_E (1 << 4)
-#define SEGMENT_F (1 << 5)
-#define SEGMENT_G (1 << 6)
-#define SEGMENT_P (1 << 7)
 
 /* === Private data type declarations ========================================================== */
 
 /* === Private variable declarations =========================================================== */
 
 /* === Private function declarations =========================================================== */
-static const uint8_t IMAGES[] = {
-    SEGMENT_A | SEGMENT_B | SEGMENT_C | SEGMENT_D | SEGMENT_E | SEGMENT_F,              //! < 0
-    SEGMENT_B | SEGMENT_C,                                                              //! < 1
-    SEGMENT_A | SEGMENT_B | SEGMENT_D | SEGMENT_E | SEGMENT_G,                          //! < 2
-    SEGMENT_A | SEGMENT_B | SEGMENT_C | SEGMENT_D | SEGMENT_G,                          //! < 3
-    SEGMENT_B | SEGMENT_C | SEGMENT_F | SEGMENT_G,                                      //! < 4
-    SEGMENT_A | SEGMENT_C | SEGMENT_D | SEGMENT_F | SEGMENT_G,                          //! < 5
-    SEGMENT_A | SEGMENT_C | SEGMENT_D | SEGMENT_E | SEGMENT_F | SEGMENT_G,              //! < 6
-    SEGMENT_A | SEGMENT_B | SEGMENT_C,                                                  //! < 7
-    SEGMENT_A | SEGMENT_B | SEGMENT_C | SEGMENT_D | SEGMENT_E | SEGMENT_F | SEGMENT_G,  //! < 8
-    SEGMENT_A | SEGMENT_B | SEGMENT_C | SEGMENT_D | SEGMENT_F | SEGMENT_G,               //! < 9
-};
+
 
 /* === Public variable definitions ============================================================= */
 
 /* === Private variable definitions ============================================================ */
 
 /* === Private function implementation ========================================================= */
-void ScreenOff(){
-    Chip_GPIO_ClearValue (LPC_GPIO_PORT, DIGITS_GPIO, DIGITS_MASK);
-    Chip_GPIO_ClearValue (LPC_GPIO_PORT, SEGMENTS_GPIO, SEGMENTS_MASK);
-}
 
-void WriteNumber(uint8_t number){
-    Chip_GPIO_SetValue (LPC_GPIO_PORT, SEGMENTS_GPIO, IMAGES[number]);
-}
-
-void SelectDigit(uint8_t digit){
-    Chip_GPIO_SetValue (LPC_GPIO_PORT, DIGITS_GPIO, (1<<digit));
-}
 
 /* === Public function implementation ========================================================= */
 
 int main(void) {
-    uint8_t valor = 0;
-    uint8_t actual = 0;
-    bool refrescar = true;
+    //uint8_t valor = 0;
+    //uint8_t actual = 0;
+    //bool refrescar = true;
 
+    uint8_t numero[4] = {1,2,3,4};
     board_t board = BoardCreate();
-
-    //WriteNumber(0);
-    //SelectDigit(0);
+    display_t display = DisplayCreate(4);
+    DisplayWriteBDC(display, numero, sizeof(numero));
 
     while (true) {
-        if(refrescar){
-            refrescar = false;
-            WriteNumber(valor);
-            SelectDigit(actual);
-        }
-        if (DigitalInputHasActivated(board->set_time)) {
-            if (valor == 9) {
-                valor = 0;
-            } else {
-                valor = valor + 1;
-            }
-            refrescar = true;
-        }
-        if (DigitalInputHasActivated(board->set_alarm)) {
-            if (valor == 0) {
-                valor = 9;
-            } else {
-                valor = valor - 1;
-            }
-            refrescar = true;
-        }
-        if (DigitalInputHasActivated(board->increment)) {
-            if (actual == 3) {
-                actual = 0;
-            } else {
-                actual = actual + 1;
-            }
-            refrescar = true;
-        }
-        if (DigitalInputHasActivated(board->decrement)) {
-            if (actual == 0) {
-                actual = 3;
-            } else {
-                actual = valor - 1;
-            }
-            refrescar = true;
-        }
+        DisplayRefresh(display);
+        
+//        if(refrescar){
+//            //refrescar = false;
+//            ScreenOff();
+//            WriteNumber(screen[actual]);
+//            SelectDigit(actual);
+//        }
+//        if (actual == 3) {
+//            actual = 0;
+//        } else {
+//            actual = actual + 1;
+//        }
+//-----------------------------------------------------------
+//        if (DigitalInputHasActivated(board->set_time)) {
+//            if (valor == 9) {
+//                valor = 0;
+//            } else {
+//                valor = valor + 1;
+//            }
+//            refrescar = true;
+//        }
+//        if (DigitalInputHasActivated(board->set_alarm)) {
+//            if (valor == 0) {
+//                valor = 9;
+//            } else {
+//                valor = valor - 1;
+//            }
+//            refrescar = true;
+//        }
+//        if (DigitalInputHasActivated(board->increment)) {
+//            if (actual == 3) {
+//                actual = 0;
+//            } else {
+//                actual = actual + 1;
+//            }
+//            refrescar = true;
+//        }
+//        if (DigitalInputHasActivated(board->decrement)) {
+//            if (actual == 0) {
+//                actual = 3;
+//            } else {
+//                actual = valor - 1;
+//            }
+//            refrescar = true;
+//        }
 
-        for (int index = 0; index < 100; index++) {
-            for (int delay = 0; delay < 25000; delay++) {
+//        for (int index = 0; index < 100; index++) {
+            for (int delay = 0; delay < 2500; delay++) {
                 __asm("NOP");
             }
-        }
+//        }
     }
 }
 
